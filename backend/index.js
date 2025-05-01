512e46cee671ae24e561b4b4d5b39450c2676d34
Joey Willman <jgwillman@wisc.edu>
2025-05-01 16:15:03 -0500

    name

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ─── Import Routes (FIXED lowercase filenames) ──────────────────────────
-const userRoutes     = require("./routes/blob");      // 
+const userRoutes     = require("./routes/user");      // 
 const locationRoutes = require("./routes/locations");
 const checkinRoutes  = require("./routes/checkin");
 
