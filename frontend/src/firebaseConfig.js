ca1d9cd8b5733e3988eaaafca96e1e50fe244b00
Joey Willman <jgwillman@wisc.edu>
2025-05-01 12:28:58 -0500

    firebase

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDbKm00OgjspICmEAdRgJ8_SeQbExXwbPA",
+  authDomain: "badger-passport.firebaseapp.com",
+  projectId: "badger-passport",
+  storageBucket: "badger-passport.firebasestorage.app",
+  messagingSenderId: "756824705680",
+  appId: "1:756824705680:web:0714ca9090df4ba201e615",
+  measurementId: "G-N62FH9RCY0"
 };
 
 const app = initializeApp(firebaseConfig);
